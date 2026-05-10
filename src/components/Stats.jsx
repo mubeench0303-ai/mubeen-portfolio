df5b15ae47de1e2c84f539db74dec5bc488c162b
mubeench0303-ai <mubeen.ch.0303@gmail.com>
2026-05-10 16:49:28 +0500

    Stats.jsx

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -43,7 +43,7 @@ const Stats = () => {
                                 className="text-4xl xl:text-6xl font-extrabold"
                             />
                             <p
-                                className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80 `}
+                                className="w-[140px] leading-snug text-white/80"
                             >
                                 {item.text}
                             </p>
